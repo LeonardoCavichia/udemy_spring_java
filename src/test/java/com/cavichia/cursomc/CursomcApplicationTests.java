d73c322f611da4fef0d984a4a82e4c69b87d1033
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-07-14 20:08:21 -0300

    Projeto Criado

---
+++ src/test/java/com/cavichia/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavichia.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

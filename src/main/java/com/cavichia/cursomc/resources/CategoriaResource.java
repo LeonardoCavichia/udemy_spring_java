de617d1d0406cc635d5918ffe156a3b900b93123
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-07-14 21:57:16 -0300

    Testando primeiro classe

--- src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,31 @@
 package com.cavichia.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cavichia.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "Rest esta funcionando";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 		
 	}
 	

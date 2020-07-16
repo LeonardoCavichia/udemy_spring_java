a30d9211e9aeae1520d5e04379b5b4ef4429c9fa
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-07-16 11:40:02 -0300

    Base de dados

--- src/main/java/com/cavichia/cursomc/domain/Categoria.java
+++ src/main/java/com/cavichia/cursomc/domain/Categoria.java
@@ -2,17 +2,23 @@ package com.cavichia.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	public Categoria() {
-		
-	
-	}
+	public Categoria() {}
 
 	public Categoria(Integer id, String nome) {
 		super();

d64665ba98c88a8297b803ccceca3722386d0964
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-07-16 19:57:19 -0300

    Ajustes no Endpoint

--- src/main/java/com/cavichia/cursomc/services/CategoriaService.java
+++ src/main/java/com/cavichia/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cavichia.cursomc.domain.Categoria;
 import com.cavichia.cursomc.repositories.CategoriaRepository;
+import com.cavichia.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+id+",Tipo" + Categoria.class.getName()));
 	}
 	
 }

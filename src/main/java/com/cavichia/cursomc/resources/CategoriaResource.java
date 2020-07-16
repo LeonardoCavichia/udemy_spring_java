94b4f542380552a0def9836d0c7a7f133629a5e0
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-07-16 12:47:07 -0300

    Criando Repository

--- src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
@@ -3,29 +3,28 @@ package com.cavichia.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cavichia.cursomc.domain.Categoria;
+import com.cavichia.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2,"Escritorio");
-		
-		List<Categoria> list = new ArrayList<>();
-		
-		list.add(cat1);
-		list.add(cat2);
-		
-		
-		return list;
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 	

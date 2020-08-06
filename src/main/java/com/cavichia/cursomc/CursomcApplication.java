719af970debcc84050fbbf719748525385224119
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-08-06 09:57:19 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/cavichia/cursomc/CursomcApplication.java
+++ src/main/java/com/cavichia/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.cavichia.cursomc.domain.Cidade;
 import com.cavichia.cursomc.domain.Cliente;
 import com.cavichia.cursomc.domain.Endereco;
 import com.cavichia.cursomc.domain.Estado;
+import com.cavichia.cursomc.domain.ItemPedido;
 import com.cavichia.cursomc.domain.Pagamento;
 import com.cavichia.cursomc.domain.PagamentoComBoleto;
 import com.cavichia.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.cavichia.cursomc.repositories.CidadeRepository;
 import com.cavichia.cursomc.repositories.ClienteRepository;
 import com.cavichia.cursomc.repositories.EnderecoRepository;
 import com.cavichia.cursomc.repositories.EstadoRepository;
+import com.cavichia.cursomc.repositories.ItemPedidoRepository;
 import com.cavichia.cursomc.repositories.PagamentoRepository;
 import com.cavichia.cursomc.repositories.PedidoRepository;
 import com.cavichia.cursomc.repositories.ProdutoRepository;
@@ -58,6 +60,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	
@@ -114,6 +119,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		
+		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
@@ -129,6 +147,8 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));		
 		
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
--- src/main/java/com/cavichia/cursomc/domain/Produto.java
+++ src/main/java/com/cavichia/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.cavichia.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -34,6 +37,9 @@ public class Produto implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Produto() {}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -43,6 +49,20 @@ public class Produto implements Serializable{
 		this.preco = preco;
 	}
 
+	
+	public List<Pedido> getPedidos() {
+		
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			
+			lista.add(x.getPedido());
+			
+		}
+		
+		return lista;
+	}
+	
+	
 	public Integer getId() {
 		return id;
 	}
@@ -73,6 +93,39 @@ public class Produto implements Serializable{
 
 	public void setCategorias(List<Categoria> categorias) {
 		this.categorias = categorias;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	

f955e3b61acc7eaea72a9f1eb30752f5aba18e20
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-08-06 10:20:20 -0300

    Atualização: Utilização somente JsonIgnore

--- src/main/java/com/cavichia/cursomc/domain/Pagamento.java
+++ src/main/java/com/cavichia/cursomc/domain/Pagamento.java
@@ -10,9 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-
 import com.cavichia.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -25,7 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId

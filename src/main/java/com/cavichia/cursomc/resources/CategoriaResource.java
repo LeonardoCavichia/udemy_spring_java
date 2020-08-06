e11486441646c7694c01c9fbde1ba9acf7513451
Leonardo Cavichia <leonardocavichia@hotmail.com>
2020-08-06 13:22:41 -0300

    Inserindo nova Categoria com post

--- src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cavichia/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,15 @@
 package com.cavichia.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cavichia.cursomc.domain.Categoria;
 import com.cavichia.cursomc.services.CategoriaService;
@@ -27,5 +28,17 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);	
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+	}
+	
 	
 }
